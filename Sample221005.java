a184e454fc1610536f1b03e8f7ce3aeba43d163d
Eunoia030 <105345279+Eunoia030@users.noreply.github.com>
2022-10-05 17:58:48 +0900

    Add files via upload

---
+++ Sample221005.java
@@ -0,0 +1,155 @@
+package test;
+
+import java.util.Scanner;
+
+public class Sample {
+
+	public static void main(String[] args) { // javac를 실행하면 가장 먼저 실행되는 부분
+			
+//		Scanner sc = new Scanner(System.in);
+//		
+//		String input = sc.nextLine();
+//		
+//		int num = Integer.parseInt(input); // 문자열(" 100:)을 정수형(100으로 변환)
+//		
+//		int num = 2147483647+1;
+//		
+//		System.out.println(num);
+//		
+//		System.out.println("input\\input");
+//		System.out.println("input\'input");
+//		System.out.println("input\ninput");  //"100" + "100"  => 100100
+//		
+//		short num1 = 32767;
+//		System.out.println((short)(num1+3));
+//      byte < short < int<long  float < double
+		float d;
+		int score = 10;
+		//
+		d = 86.5f + score;
+		//86.5 + 10.0
+		System.out.println(d);
+	
+		
+		
+		
+	 	
+	// 자료형에는 기본형, 참조형이 있다.
+		
+	// 기본형 : 정수(short, int , long), 실수(float, double)
+    //     -크기::  2      4     8          4       8
+		
+		
+	// int (자료형) num1 (변수명)
+		
+//		int num1 = 10;
+//		int num2 = 20;
+//		
+//		int result;
+//		
+//		result = num1+ num2;
+//		
+//		System.out.println("result: " + result);
+		
+		
+//		int age =0;
+//		
+//		age =age +20;
+//		
+//		System.out.println("age: "+age);
+//		
+//		System.out.println("Hello World!!!");  //출력 부분
+//		
+//		System.out.println("이젠컴퓨터아카데미");
+		
+		// 변수 초기화
+		
+//		int year = 0;
+//		int age = 14;
+//		
+//		System.out.println(year);
+//		System.out.println(age);
+//		
+//		
+//		
+//		year = age + 2000;
+//		age = age + 1;
+//		
+//		System.out.println(year);
+//		System.out.println(age);
+		
+		// 변수값 교환하기
+		
+//		int x =10;
+//		int y =20;
+//		int temp; //임시 변수값 지정
+//		
+//		System.out.println("before => x:"+x+",y:"+y);
+//		
+//		temp =x;
+//		x= y;
+//		y = temp;
+//		
+//		System.out.println("before => x:"+x+",y:"+y);
+//		
+		//ctrl + alt + ↓ 같은것이 아래에 자동 입력
+		
+		//sysout+ctrl+spacebar =System.out.println();  -> 단축키
+		
+//		 cmd :::: dir = 목록 확인
+//		          dir/p = 나눠서 목록 확인
+//		          cls =  화면 clear
+//		          cd.. = 전 단계 이동
+//		          cd 폴더이름 = 해당 폴더로 이동
+//		          드라이브이름:  = 해당 드라이브로 변경 (예)c:\>d:
+//		          일부파일명입력후 + tab ==풀파일명으로 자동 입력
+//		          md 폴더명 = 해당폴더명의 폴더생성
+//                rd 폴더명 = 해당폴더 삭제
+		
+		
+		
+		// 아스키코드 
+		// 예시: HELLO 
+		//		1001000 H
+		//		1100110 E
+		//		1101100 L
+		//		1101100 L
+		//		01101111 O
+		
+		//컴퓨터의 실수 처리는 불안정하다. 
+		//Ex) 0.1 x 10 = 1 그러나, 처리를 해보면 0.9999999..?
+//		
+//		double sum = 0;
+//		
+//		int total = 100;
+//		
+//		for(int i=0; i<100;i++) {
+//			sum = sum + 0.1;
+//		}
+//		System.out.printf("sum: %f", sum);
+//		System.out.printf("total: %d", total);
+		
+		
+		//final =  수정할수없도록 고정시키는 명령어 = 상수화
+		
+//		final int AGE = 20;  //보통 고정시킬때, 대문자로 표기함 
+//	
+//		System.out.println(AGE);
+		
+		
+//		char ch = '7';  // "" 사용 불가능 , 두 문자 이상 불가능 // char 자료형에는 문자 1개만 대입가능 = 숫자가 오더라도 문자로 인식 
+//		String name = "java"; // 문자열 즉, 한개 이상 모여있는 것이 가능
+//		
+//		System.out.println(ch + "," + name);
+//		
+//		System.out.printf("%c", ch);
+//		
+		
+		
+	}
+
+}
+
+//class Sample2{
+//	
+//}
